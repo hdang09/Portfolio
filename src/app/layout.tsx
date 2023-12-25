ebf0fa9eecf99c135921d44b0bf31e5f04758d9d
hdang09 <dangtranhai628@gmail.com>
2023-12-25 22:42:22 +0700

    ADD - Add information for metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,43 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { title, description, icons, BASE_URL } from './metadata';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Tran Hai Dang - Full-stack Developer',
-    description:
-        'My name is Hai Dang. I am a full-stack developer and designer, currently studying Bachelor of Science and majoring in Software Engineering at FPT University.',
+    title,
+    description,
+    openGraph: {
+        type: 'website',
+        url: BASE_URL,
+        title,
+        description,
+        images: [
+            {
+                url: BASE_URL + '/og.png',
+                width: 1200,
+                height: 630,
+                alt: title,
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title,
+        description,
+        images: [
+            {
+                url: BASE_URL + '/og.png',
+                width: 1200,
+                height: 630,
+                alt: title,
+            },
+        ],
+    },
+    icons,
+    manifest: BASE_URL + '/favicon/manifest.json',
+    applicationName: 'Tran Hai Dang Portfolio',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
---
+++ src/app/metadata.ts
@@ -0,0 +1,119 @@
+export const title = 'Tran Hai Dang - Full-stack Developer';
+
+export const description =
+    'My name is Hai Dang. I am a full-stack developer and designer, currently studying Bachelor of Science and majoring in Software Engineering at FPT University.';
+
+export const BASE_URL = 'https://www.hdang09.tech';
+
+export const icons = [
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '57x57',
+        href: '/favicon/apple-icon-57x57.png',
+        url: BASE_URL + '/favicon/apple-icon-57x57.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '60x60',
+        href: '/favicon/apple-icon-60x60.png',
+        url: BASE_URL + '/favicon/apple-icon-60x60.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '72x72',
+        href: '/favicon/apple-icon-72x72.png',
+        url: BASE_URL + '/favicon/apple-icon-72x72.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '76x76',
+        href: '/favicon/apple-icon-76x76.png',
+        url: BASE_URL + '/favicon/apple-icon-76x76.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '114x114',
+        href: '/favicon/apple-icon-114x114.png',
+        url: BASE_URL + '/favicon/apple-icon-114x114.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '120x120',
+        href: '/favicon/apple-icon-120x120.png',
+        url: BASE_URL + '/favicon/apple-icon-120x120.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '144x144',
+        href: '/favicon/apple-icon-144x144.png',
+        url: BASE_URL + '/favicon/apple-icon-144x144.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '152x152',
+        href: '/favicon/apple-icon-152x152.png',
+        url: BASE_URL + '/favicon/apple-icon-152x152.png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        type: 'image/png',
+        sizes: '180x180',
+        href: '/favicon/apple-icon-180x180.png',
+        url: BASE_URL + '/favicon/apple-icon-180x180.png',
+    },
+    {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/favicon/android-icon-192x192.png',
+        url: BASE_URL + '/favicon/android-icon-192x192.png',
+    },
+    {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+        url: BASE_URL + '/favicon/favicon-32x32.png',
+    },
+    {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicon/favicon-96x96.png',
+        url: BASE_URL + '/favicon/favicon-96x96.png',
+    },
+    {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+        url: BASE_URL + '/favicon/favicon-16x16.png',
+    },
+    {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '144x144',
+        href: '/favicon/ms-icon-144x144.png',
+        url: BASE_URL + '/favicon/ms-icon-144x144.png',
+    },
+    {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.png',
+        url: BASE_URL + '/favicon.png',
+    },
+    {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        href: '/favicon.svg',
+        url: BASE_URL + '/favicon.svg',
+    },
+];
